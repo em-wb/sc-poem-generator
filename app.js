93ccd0e25186532e6eecff13517e36c322a69d56
em-wb <131539794+em-wb@users.noreply.github.com>
2023-12-04 19:53:53 +0100

    Create new function for api call and reset form

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function getResponse(response) {
+function handleResponse(response) {
   new Typewriter("#output-text", {
     strings: response.data.answer,
     autoStart: true,
@@ -8,20 +8,19 @@ function getResponse(response) {
 }
 
 function getApiInfo() {
-  return url;
+  let protagonist = document.getElementById("protagonist").value;
+  let topic = document.getElementById("topic").value;
+  const apiKey = "f9a74e38f278fb4dtffa389bo754ad40";
+  const context =
+    "You are an AI assistant who loves creating five-line poems in the style of a limerick that are lighthearted and fun. Please ensure each line of the poem is wrapped in a <p></p> element.";
+  const prompt = `Please write a poem about ${protagonist} on the topic ${topic}`;
+  const url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(url).then(handleResponse);
+  document.getElementById("form").reset();
 }
 
 document.querySelector("button").addEventListener("click", (e) => {
   e.preventDefault();
-  const protagonist = document.getElementById("protagonist").value;
-  const topic = document.getElementById("topic").value;
-  const apiKey = "f9a74e38f278fb4dtffa389bo754ad40";
-  const context =
-    "You are an AI assistant who loves creating five-line poems in the style of an Irish limerick that are lighthearted and fun. Please ensure each line of the poem is wrapped in a <p></p> element.";
-  const prompt = `Please write a poem about ${protagonist} on the topic ${topic}`;
-  const url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-  console.log("hey", prompt, url);
   document.getElementById("output-container").classList.remove("hidden");
   new Typewriter("#loading", {
     strings: "...",
@@ -30,5 +29,5 @@ document.querySelector("button").addEventListener("click", (e) => {
     autoStart: true,
     cursor: "",
   });
-  axios.get(url).then(getResponse);
+  getApiInfo();
 });
